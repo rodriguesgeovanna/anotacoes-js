8753cfafc279f2764e2557c0d5cc09cfa0234c1b
Geovanna <138888795+rodriguesgeovanna@users.noreply.github.com>
2023-08-22 21:51:48 -0300

    Add files via upload

---
+++ Untitled-2.js
@@ -0,0 +1,57 @@
+//let aluno = ["Zé", "Joana", "Sarah"];
+
+//let nota1 = 1;
+//let nota2 = 3;
+//let nota3 = 8.5;
+
+
+/* método push adiciona um novo elemento no final da matriz */
+//aluno.push("Zerbin");
+//aluno.push("Senai");
+//aluno.push("Geovanna");
+//aluno.push("Noite");
+//Métod unshift() - Incluir um novo elemento no inicio do array
+//aluno.unshift("Dia");
+//Método pop() Que remove o último
+//aluno.pop();
+//console.log(aluno[0])
+/* Método shift() - remove o primeiro elemento */
+//console.log("tamanho da Matriz :" + aluno.length);
+
+//console.table(aluno);
+//Inserir na posição um o elemento do carmo
+//aluno[1] = "Maria Antonieta"
+//delete aluno[2];
+
+
+//console.table(aluno);
+//console.log(aluno.indexOf("maria"))
+//adicionar o elemento em uma posição específica//
+//aluno.splice(2,0,"Guarulhos")
+
+//console.log(aluno[0])
+
+/*  let cadastro = [
+["ID :",9],
+["NOME: ","Geovanna"],
+["ENDEREÇO : ", "Av. da Saudade, 125"],
+["CIDADE: ", "Campinas-Sp"],
+["TELEFONE: ", 123456789809]
+]; */
+
+
+let cadastro = [
+["Geovanna", 10, 9, 10, 9],
+["Josiane", 12,10,12,5],
+["Pedro", 10,8.5,9.5,10],
+["Ana", 5,5.5,6,7],
+["Claudia", 19,3.6,8.5,2.5]
+];
+
+
+
+
+console.table(cadastro);
+
+console.log(cadastro[2][1])
+
